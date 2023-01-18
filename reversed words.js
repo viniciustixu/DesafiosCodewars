b2cd4cdfed20dec6cdbe04c5f05ab36972ec05c4
viniciustixu <vinicipkt@live.com>
2023-01-18 17:35:04 -0300

    reversed words

---
+++ reversed words.js
@@ -0,0 +1,23 @@
+/* Complete the solution so that it reverses all of the words within the string passed in.
+
+Words are separated by exactly one space and there are no leading or trailing spaces.
+
+Example(Input --> Output):
+
+"The greatest victory is that which requires no battle" --> "battle no requires which that is victory greatest The" */
+
+
+
+
+
+
+
+function reverseWords(str){
+    let c = str.split(' ')
+    let f = []
+    for (let i = c.length; i > 0 ; i-- ) {
+      f.push(c[i-1])
+    }
+    let g = f.join(" ");
+    return g
+  }
