076d78b4ed177c8836082cf9901452adcce46dcf
viniciustixu <vinicipkt@live.com>
2023-01-22 23:19:36 -0300

    Update

--- Filter out the geese.js
+++ Filter out the geese.js
@@ -1,4 +1,4 @@
-Write a function that takes a list of strings as an argument and returns a filtered list containing the same elements but with the 'geese' removed.
+/*Write a function that takes a list of strings as an argument and returns a filtered list containing the same elements but with the 'geese' removed.
 
 The geese are any strings in the following array, which is pre-populated in your solution:
 
@@ -9,4 +9,4 @@ For example, if this array were passed as an argument:
 Your function would return the following array:
 
 ["Mallard", "Hook Bill", "Crested", "Blue Swedish"]
-The elements in the returned array should be in the same order as in the initial array passed to your function, albeit with the 'geese' removed. Note that all of the strings will be in the same case as those provided, and some elements may be repeated.
+The elements in the returned array should be in the same order as in the initial array passed to your function, albeit with the 'geese' removed. Note that all of the strings will be in the same case as those provided, and some elements may be repeated.*/
